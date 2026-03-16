00461c8117bc1cbe3eae1dba6b961d8a3f110ace
zoshesky-uaa <zoshesky@alaska.edu>
2026-03-15 22:41:49 -0800

    Iteration and filewriting change

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 ### Check List
 * Information regarding delay and other possible issues with KMixxer:
 https://github.com/PortAudio/portaudio/wiki/Win32AudioBackgroundInfo
+
+Bugs:
+Fix thhe frame index allocation for the numpy memorymap of the feature tesnors during the simulation.
--- run/traffic.py
+++ run/traffic.py
@@ -1,7 +1,6 @@
-from time import sleep
 import math
-from const import MINIMUM_TRAFFIC_VEHICLES, MAXIMUM_TRAFFIC_VEHICLES, MINIMUM_EMERGENCY_VEHICLES, MAXIMUM_EMERGENCY_VEHICLES 
 import const
+
 """
 SoundEvent class to represent audio events in the simulation
 Class Events:
@@ -25,13 +24,12 @@ class VehicleSoundEvent:
         self.fsm = fsm
         self.vehicle = vehicle
         self.tick = tick
-        self.wait()
-
-    def wait(self):
-        self.vehicle.set_license_plate("TRAFFIC")
-        self.normal_behavior()
-        self.tick.wait_next() 
         self.run()
+
+    def run(self):
+        self.vehicle.set_license_plate("TRAFFIC")
+        self.tick.waited_action(self.normal_behavior)
+        self.tick.waited_action_iterate()
 
     def normal_behavior(self):
         self.vehicle.ai.set_aggression(0.2)
@@ -39,12 +37,7 @@ class VehicleSoundEvent:
         self.vehicle.ai.set_speed(15.65, mode="limit")
         self.vehicle.ai.set_mode("traffic")
 
-    def run(self):
-        while self.tick.frame_index < const.END_FRAME and not self.tick.shutdown.is_set():
-            sleep(0.1)
- 
-
-    def relative_position_data(self):
+    def position_data(self, relative=False):
         self.simulation.vehicle_controller.driver.sensors.poll()  
         self.vehicle.sensors.poll()     
         origin_position = self.simulation.vehicle_controller.driver.state['pos']  
@@ -55,15 +48,19 @@ class VehicleSoundEvent:
         dz = origin_position[2] - sound_position[2]
         magnitude = math.sqrt(dx**2 + dy**2 + dz**2)
 
-        if magnitude > 0:
+        if (magnitude > 0) and relative:
             return (dx / magnitude, dy / magnitude, dz / magnitude)
-        else:
+        elif relative:
             return (0.0, 0.0, 0.0)
+        else:
+            return (magnitude)
     
-    def write_soundevent(self):
-        position = self.relative_position_data()
-        self.fsm.write_soundevent_csv(self.class_index, self.track_index, position, self.tick.frame_index)       
-
+    def write_event(self):
+        position = self.position_data(relative=True)
+        self.fsm.write_soundevent_csv(self.track_index, position)       
+    
+    def write_reset(self):
+        self.fsm.write_soundevent_csv(self.track_index, (0.0, 0.0, 0.0))
     '''
     # Horns can't be triggered via BeamNGpy
     def random_honk_event(self):
